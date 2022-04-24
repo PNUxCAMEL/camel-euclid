3c811704a67ea4d212c853304fce9a08ef648148
wookjinAhn <lexqrt2@pusan.ac.kr>
2022-04-24 15:00:57 +0900

    modified CMakeLists.txt

---
+++ vector/include/Vector.h
@@ -0,0 +1,11 @@
+//
+// Created by wj on 22. 4. 24.
+//
+
+#ifndef CAMEL_EUCLID_VECTOR_H
+#define CAMEL_EUCLID_VECTOR_H
+
+#include "Vector2D.h"
+#include "Vector3D.h"
+
+#endif //CAMEL_EUCLID_VECTOR_H
